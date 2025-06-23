5869bb8d23c4e3f75bceb6debbd801c200ceea0f
gakutasu <gakutasu@gmail.com>
2025-06-23 19:54:41 +0900

    add cyclonedds to sysctl

--- setup.sh
+++ setup.sh
@@ -1,13 +1,9 @@
 #!/bin/sh
 
+DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
+
 symlink_dotfiles() {
-    DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
-    FILES_TO_LINK="
-        .bashrc
-        10-cyclone-max.conf
-        cyclonedds.xml
-        ros2_alias.sh"
-    
+    FILES_TO_LINK=".bashrc cyclonedds.xml ros2_alias.sh"
     for fname in $FILES_TO_LINK; do
         src="$DOTFILES_DIR/$fname"
         dest="$HOME/$fname"
@@ -15,8 +11,22 @@ symlink_dotfiles() {
     done
 }
 
+add_sysctl_conf() {
+    CONF_FILE="$DOTFILES_DIR/10-cyclone-max.conf"
+    if [ -f "$CONF_FILE" ]; then
+        echo "Adding $CONF_FILE to /etc/sysctl.conf ..."
+        sudo tee -a /etc/sysctl.conf < "$CONF_FILE"
+        echo "Applying sysctl settings ..."
+        sudo sysctl -p
+    else
+        echo "$CONF_FILE not found."
+        return 1
+    fi
+}
+
 main() {
     symlink_dotfiles
+    add_sysctl_conf
 }
 
 main "$@"
